65c4a27cc9bc75fff6f778fbbd574864751382bc
Syed Shuja Hussain Shah <shuja@alitech.cloud>
2022-12-30 21:36:40 +0500

    Finalize Overlay

--- src/Componenet/Card.js
+++ src/Componenet/Card.js
@@ -1,15 +1,15 @@
-import { Box, Grid } from '@mui/material';
-import './card.css';
-import { useState, useEffect } from 'react';
-import Typography from '@mui/material/Typography';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import Rating from '@mui/material/Rating';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import image1 from '../Assets/Image1.jpg';
-import PercentageB from '../Assets/PercentageB.svg';
-import PercentageW from '../Assets/PercentageW.png';
+import { Box, Grid } from "@mui/material";
+import "./card.css";
+import { useState, useEffect } from "react";
+import Typography from "@mui/material/Typography";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import Rating from "@mui/material/Rating";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import image1 from "../Assets/Image1.jpg";
+import PercentageB from "../Assets/PercentageB.svg";
+import PercentageW from "../Assets/PercentageW.png";
 
 const CountDown = () => {
   const [days, setDays] = useState(78);
@@ -51,20 +51,20 @@ const CountDown = () => {
     <Grid container justifyContent="space-around" alignItems="center">
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          gap: '0.5rem',
-          color: '#fff',
-          fontFamily: 'Lato',
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "0.5rem",
+          color: "#fff",
+          fontFamily: "Lato",
 
           letterSpacing: 3,
         }}
       >
         <Typography
           variant="h4"
-          sx={{ color: '#fff', fontFamily: 'Lato', letterSpacing: 3 }}
+          sx={{ color: "#fff", fontFamily: "Lato", letterSpacing: 3 }}
         >
           {days < 10 ? `0${days}` : days}
         </Typography>
@@ -72,63 +72,59 @@ const CountDown = () => {
       </Box>
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          gap: '0.5rem',
-          color: '#fff',
-          fontFamily: 'Lato',
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "0.5rem",
+          color: "#fff",
+          fontFamily: "Lato",
           letterSpacing: 3,
         }}
       >
         <Typography
           variant="h4"
-          sx={{ color: '#fff', fontFamily: 'Lato', letterSpacing: 3 }}
+          sx={{ color: "#fff", fontFamily: "Lato", letterSpacing: 3 }}
         >
-          {hours < 10 ? `0${hours}` : hours}
-          {' '}
-          :
+          {hours < 10 ? `0${hours}` : hours} :
         </Typography>
         HOURS
       </Box>
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          gap: '0.5rem',
-          color: '#fff',
-          fontFamily: 'Lato',
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "0.5rem",
+          color: "#fff",
+          fontFamily: "Lato",
           letterSpacing: 3,
         }}
       >
         <Typography
           variant="h4"
-          sx={{ color: '#fff', fontFamily: 'Lato', letterSpacing: 3 }}
+          sx={{ color: "#fff", fontFamily: "Lato", letterSpacing: 3 }}
         >
-          {minutes < 10 ? `0${minutes}` : minutes}
-          {' '}
-          :
+          {minutes < 10 ? `0${minutes}` : minutes} :
         </Typography>
         MINUTES
       </Box>
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          gap: '0.5rem',
-          color: '#fff',
-          fontFamily: 'Lato',
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "0.5rem",
+          color: "#fff",
+          fontFamily: "Lato",
           letterSpacing: 3,
         }}
       >
         <Typography
           variant="h4"
-          sx={{ color: '#fff', fontFamily: 'Lato', letterSpacing: 3 }}
+          sx={{ color: "#fff", fontFamily: "Lato", letterSpacing: 3 }}
         >
           {seconds}
         </Typography>
@@ -147,20 +143,32 @@ const CardData = () => {
       alignItems="center"
       direction="column"
       sx={{
-        height: '100%',
-        width: '100%',
-        background: 'rgba(0,0,0,0.8)',
-        borderTopLeftRadius: 4,
-        borderTopRightRadius: 4,
-        gap: '1rem',
+        height: "100%",
+        width: "100%",
+        position: "relative",
+        background:
+          "linear-gradient(180deg, rgba(0,0,0,0.2) 0%, rgba(0,0,0,0.8) 100%)",
+        borderRadius: "1rem",
+        gap: "1rem",
+
+        "&::before": {
+          content: "''",
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          top: 0,
+          left: 0,
+          background: `url(${image1}) no-repeat center center/cover`,
+          borderRadius: "1rem",
+          zIndex: -1,
+        },
       }}
     >
       <Grid
         item
-        xs={12}
         sx={{
-          height: '100%',
-          width: '100%',
+          height: "100%",
+          width: "100%",
           maxWidth: 800,
         }}
       >
@@ -172,43 +180,43 @@ const CardData = () => {
         justifyContent="flex-end"
         alignItems="center"
         sx={{
-          padding: '1rem',
+          padding: "1rem",
         }}
       >
         <Rating
           max={1}
           icon={<FavoriteIcon fontSize="inherit" />}
-          emptyIcon={(
+          emptyIcon={
             <FavoriteBorderIcon
               fontSize="inherit"
               color="#fff"
               precision={0.5}
             />
-          )}
+          }
         />
       </Grid>
       <Grid item container justifyContent="space-between" alignItems="center">
         <ChevronLeftIcon
           sx={{
-            color: '#fff',
+            color: "#fff",
             fontSize: 50,
-            cursor: 'pointer',
-            '&:hover': {
-              color: '#fff',
-              transform: 'scale(1.2)',
-              transition: 'transform 0.5s',
+            cursor: "pointer",
+            "&:hover": {
+              color: "#fff",
+              transform: "scale(1.2)",
+              transition: "transform 0.5s",
             },
           }}
         />
         <ChevronRightIcon
           sx={{
-            color: '#fff',
+            color: "#fff",
             fontSize: 50,
-            cursor: 'pointer',
-            '&:hover': {
-              color: '#fff',
-              transform: 'scale(1.2)',
-              transition: 'transform 0.5s',
+            cursor: "pointer",
+            "&:hover": {
+              color: "#fff",
+              transform: "scale(1.2)",
+              transition: "transform 0.5s",
             },
           }}
         />
@@ -219,29 +227,29 @@ const CardData = () => {
             src={PercentageW}
             alt="percentage"
             style={{
-              width: '150px',
-              height: 'auto',
+              width: "150px",
+              height: "auto",
             }}
           />
         </Grid>
         <Grid item>
           <Box
             style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-              padding: '1rem',
-              width: '250px',
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              padding: "1rem",
+              width: "250px",
             }}
           >
             <h4
               style={{
-                color: '#fff',
-                fontFamily: 'Lato',
-                fontSize: '0.8rem',
-                letterSpacing: '0.5px',
-                textTransform: 'uppercase',
-                fontWeight: '400',
+                color: "#fff",
+                fontFamily: "Lato",
+                fontSize: "0.8rem",
+                letterSpacing: "0.5px",
+                textTransform: "uppercase",
+                fontWeight: "400",
               }}
             >
               $ 5,000,000
@@ -249,11 +257,11 @@ const CardData = () => {
 
             <h4
               style={{
-                color: '#fff',
-                fontFamily: 'Lato',
-                fontSize: '1rem',
-                letterSpacing: '-0.5px',
-                fontWeight: '500',
+                color: "#fff",
+                fontFamily: "Lato",
+                fontSize: "1rem",
+                letterSpacing: "-0.5px",
+                fontWeight: "500",
               }}
             >
               Sotheby&apos;s
@@ -267,8 +275,8 @@ const CardData = () => {
         justifyContent="space-between"
         alignItems="center"
         sx={{
-          padding: '1rem',
-          width: '100%',
+          padding: "1rem",
+          width: "100%",
         }}
       >
         <h4 className="typography">Shelton Streen</h4>
@@ -280,9 +288,9 @@ const CardData = () => {
       <div
         item
         style={{
-          width: '100%',
-          height: '2rem',
-          backgroundColor: '#fff',
+          width: "100%",
+          height: "2rem",
+          backgroundColor: "#fff",
         }}
       />
       <Grid
@@ -291,8 +299,8 @@ const CardData = () => {
         justifyContent="space-between"
         alignItems="center"
         sx={{
-          padding: '1rem',
-          width: '100%',
+          padding: "1rem",
+          width: "100%",
           borderBottomLeftRadius: 4,
           borderBottomRightRadius: 4,
         }}
@@ -300,7 +308,6 @@ const CardData = () => {
         <h4 className="typography2">$25.0 USD</h4>
         <h4 className="typography2">BUY Entry now</h4>
       </Grid>
-
     </Grid>
   );
 };
@@ -308,15 +315,28 @@ const CardData = () => {
 const Card = () => (
   <section
     style={{
-      height: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      gap: '0.5rem',
-      color: '#fff',
+      height: "100%",
+      width: "600px",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      gap: "0.5rem",
+      color: "#fff",
+      padding: "2rem",
+      margin:"30px auto",
     }}
   >
+    {/* <img
+      src={image1}
+      style={{
+        width: "150px",
+        height: "auto",
+        objectFit: "cover",
+        objectPosition: "center",
+
+      }}
+    /> */}
     <CardData />
   </section>
 );
