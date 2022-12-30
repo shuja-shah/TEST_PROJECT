3c434f3c894478f9259bef04b7864911de090412
shuja-shah <shujashah786@gmail.com>
2022-12-30 08:34:09 -0800

    Finalize countdown

--- src/Componenet/Card.js
+++ src/Componenet/Card.js
@@ -1,6 +1,106 @@
-import { Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import "./card.css";
 import image1 from "../Assets/Image1.jpg";
+import { useState, useEffect } from "react";
+import Typography from "@mui/material/Typography";
+
+const CountDown = () => {
+  const [days, setDays] = useState(78);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          if (hours === 0) {
+            if (days === 0) {
+              clearInterval(interval);
+            } else {
+              setDays(days - 1);
+              setHours(23);
+              setMinutes(59);
+              setSeconds(59);
+            }
+          } else {
+            setHours(hours - 1);
+            setMinutes(59);
+            setSeconds(59);
+          }
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [seconds, minutes, hours, days]);
+
+  return (
+    <Grid container justifyContent="space-around" alignItems="center">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "0.5rem",
+        }}
+      >
+        <Typography variant="h4" sx={{ color: "#fff", fontFamily: "Lato" }}>
+          {days < 10 ? "0" + days : days}:{" "}
+        </Typography>
+        DAYS
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "0.5rem",
+        }}
+      >
+        <Typography variant="h4" sx={{ color: "#fff", fontFamily: "Lato" }}>
+          {hours < 10 ? "0" + hours : hours} :
+        </Typography>
+        HOURS
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "0.5rem",
+        }}
+      >
+        <Typography variant="h4" sx={{ color: "#fff", fontFamily: "Lato" }}>
+          {minutes < 10 ? "0" + minutes : minutes} :
+        </Typography>
+        MINUTES
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "0.5rem",
+        }}
+      >
+        <Typography variant="h4" sx={{ color: "#fff", fontFamily: "Lato" }}>
+          {seconds}
+        </Typography>
+        SECONDS
+      </Box>
+    </Grid>
+  );
+};
 
 const Card = () => (
   <Grid
@@ -20,9 +120,10 @@ const Card = () => (
         width: "100%",
         boxShadow: "0 0 10px #000",
         maxWidth: 800,
+        background: "linear-gradient(90deg, #000000 0%, #434343 100%)",
       }}
     >
-      <img
+      {/* <img
         alt="cardImg"
         src={image1}
         style={{
@@ -31,10 +132,10 @@ const Card = () => (
           objectFit: "contain",
           objectPosition: "center",
           borderRadius: 4,
-          // border: "1px solid #000",
           boxShadow: "0 0 10px #000",
         }}
-      />
+      /> */}
+      <CountDown />
     </Grid>
   </Grid>
 );
